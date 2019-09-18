104da7512789b89be9dfd888fec479c55ecb9922
Alex Farmer <farmale10@gmail.com>
2019-09-18 02:20:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,10 @@
 
-class Dog 
-  
-  def this_dogs_name
-    
-  end 
-end 
-
-lassie = Dog.new
+class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+  end
+end
